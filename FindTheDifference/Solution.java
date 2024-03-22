f0cda2a45102d5a9fd6cf5bb9b247ebedd55e175
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-03-22 18:22:40 +0400

    finally final soluction

--- FindTheDifference/Solution.java
+++ FindTheDifference/Solution.java
@@ -3,36 +3,45 @@ class Solution {
         
         char[] ss = s.toCharArray();
         char[] tt = t.toCharArray();
+        char[] alphabetic = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        int[] alphaNumb = new int[26];
 
-        
-
-        
-        if (s.length() != 0){
-            for(int index=0; index<t.length(); index++){
-                for(int index02=0; index02<s.length(); index02++){
-                    
-                    if(tt[index] == ss[index02]){
-                        break;
-                    }
-                    
-                    
-                    if (index02 == (s.length()-1)){
-                        
-                        return tt[index];
-                    }
-                    
+        for(int i =0; i<tt.length; i++){
+            for(int j=0; j<alphabetic.length; j++){
+                if(tt[i] == alphabetic[j]){
+                    alphaNumb[j]+=1;
                 }
             }
         }
 
 
-        return t.charAt(0);
-    }
+        for(int i=0; i<ss.length; i++){
+            for(int j=0; j<alphabetic.length; j++){
+                if(ss[i] == alphabetic[j] && alphaNumb[j] > 0){
+                    alphaNumb[j]-=1;}
+                // }else{
+                //     return alphabetic[j];
+                // }
+            }
+        }
 
+
+
+
+       for(int i=0; i<alphaNumb.length; i++){
+        if(alphaNumb[i] ==1){
+            return alphabetic[i];
+        }
+       }
+       return 0;
+    }
+ 
 
     public static void main(String args[]){
 
-        System.out.println(findTheDifference("abcd", "abcde"));
+    System.out.println(findTheDifference("ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor", "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj"));
+
+        // System.out.println(findTheDifference("abcd", "abcde"));
 
     }
 }
