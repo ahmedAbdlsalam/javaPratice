394affd0c2ee039f775f020efb8cbe8f9fca2d4f
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-04-12 15:09:22 +0200

    Initial Commit for adding third problem

---
+++ Proplem03/code.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int strStr(String haystack, String needle) {
+        char[] haystackarray = haystack.toCharArray();
+        char[] needlearray = needle.toCharArray();
+        
+        
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello Ahmed");
+    }
+}
