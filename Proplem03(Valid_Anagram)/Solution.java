e83805fe39e4edae36c78ba546c644d2aea340bf
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-04-26 18:21:53 +0300

    initial commit for Proplem03(Valid_anagram)

---
+++ Proplem03(Valid_Anagram)/Solution.java
@@ -0,0 +1,65 @@
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class Solution {
+    
+
+    public static Dictionary charOccurrence(String data){
+        char[] dataChar = data.toCharArray();
+
+        Hashtable<Character, Integer> occurrence = new Hashtable<>();
+        occurrence.put('a', 1);
+        occurrence.put('b', 1);
+        occurrence.put('c', 1);
+        for(int i=0; i < dataChar.length; i++){
+            char[] keys = occurrence.keySet().toArray();
+            for (int j=0; j<occurrence.keySet().size(); j++){
+                if(dataChar[i] == (toCharArray)occurrence.keySet().[i])
+            }
+        }
+
+        System.out.println(occurrence.keySet());
+  
+        // for (Character key: occurrence.keySet())
+        // {
+        //     System.out.println(key +"\b" + occurrence.get(key));
+        // }
+
+
+        return occurrence;
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+        
+        // if (s.length() != t.length()){
+        //     return false;
+        // }
+        // for (int i =0 ; i<t.length(); i++){
+        //     for (int j=0; j<s.length(); j++){
+        //         if((tt[i] != ss[j]) && j == s.length()-1){
+        //             return false;
+        //         }
+
+        //         if((tt[i] == ss[j])){
+        //             break;
+        //         }
+
+        //     }
+        // }
+        
+        return true;
+    }
+
+
+    public static void main(String args[]){
+
+        // System.out.println(isAnagram("rat", "car"));
+        charOccurrence("Ahmed");
+        
+    
+    }
+}
---
+++ Proplem03(Valid_Anagram)/test.java
@@ -0,0 +1,18 @@
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class test {
+    
+
+    public static void main(String[] args) {
+        Hashtable<Character,Integer> table= new Hashtable<>();
+
+
+        table.put('a', 1);
+        table.put('c', 2);
+
+        
+
+    }
+}
