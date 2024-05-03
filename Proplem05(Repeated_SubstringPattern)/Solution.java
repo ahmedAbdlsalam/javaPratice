5617ce049f2661dc7d63eecb83cb8a75fe000825
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-05-03 14:06:54 +0300

    Initial commit for Problem05

---
+++ Proplem05(Repeated_SubstringPattern)/Solution.java
@@ -0,0 +1,9 @@
+// package Proplem05(Repeated_SubstringPattern);
+
+public class Solution {
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello mode");
+    }
+}
