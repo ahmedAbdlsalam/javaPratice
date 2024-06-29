92ba0d0f41f9771f7b829abf54b3e9c9981a3914
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-06-29 17:35:19 +0300

    solution

--- Proplem05(Repeated_SubstringPattern)/Solution.java
+++ Proplem05(Repeated_SubstringPattern)/Solution.java
@@ -2,29 +2,33 @@
 
 public class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        
-        if(s.length()%2 !=0 ) return false;
 
-        for (int i=0; i < s.length()/2; i++){
-            // System.out.println(i);
-            // System.out.println(s.length()/2+i);
-            // System.out.println(s.charAt(i));
-            // System.out.println(s.charAt(s.length()/2+i));
-            if(s.charAt(i) != s.charAt(s.length()/2+i)) return false;
+        for(int i=1; i<=s.length()/2; i++){
+            if(s.length()%i ==0){
+                StringBuilder battern= new StringBuilder();
+
+            for(int j=0; j<s.length()/i; j++){
+                battern.append(s.substring(0, i));
+            }
+            System.out.println(battern);
+            System.out.println("==============");
+            if(s.equals(battern.toString())) return true;
         }
+            }
+            
 
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
         System.out.println("Hello mode");
 
         String x = "abcabcabcabc";
-        String y = "abcabcabcabc";
+        // String y = "abcabcabcabc";
         
-        System.out.println( (x==y) ?  true:  false);
-        // Solution s = new Solution();
-        // System.out.println(s.repeatedSubstringPattern(x));
+        // System.out.println( (x==y) ?  true:  false);
+        Solution s = new Solution();
+        System.out.println(s.repeatedSubstringPattern(x));
 
     }
 }
