2fc3c1e0e460723ab991ba4e0a7f0ad325121f25
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-10 08:15:05 +0300

    Adding new problem8(Sign of the product of an array

--- Proplem07(Plus one)/Solution.java
+++ Proplem07(Plus one)/Solution.java
@@ -1,14 +1,43 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
-        
+        int n = digits.length;
+       for(int i=n-1 ; i>=0; i--){
+            if(digits[i] == 9){
+                digits[i] = 0;
+            }else{
+                digits[i]+=1;
+                return digits;
+            }
+       }
+        digits = new int[n+1];
+        digits[0] = 1;
+
+
+
+
+        return digits;
     }
 
 
+public int[] AddNewItem(int[] arr){
+    int[] arr2 = new int[arr.length + 1];
 
+    for(int i =0; i<arr.length; i++){
+        arr2[i] = arr[i];
+    }
+
+    return arr2;
+}
 
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Solution s = new Solution();
+        int[] array = new int[]{4,3,2,9};
+        int[] result = s.plusOne(array);
+
+        for (int i : result) {
+            System.out.println(i);
+        }
     }
 }
---
+++ Proplem08(SignoftheProductofanArray)/Solution.java
@@ -0,0 +1,9 @@
+
+public class Solution {
+    public int arraySign(int[] nums) {
+        int SignProduct=0;
+        
+
+        return SignProduct;
+    }
+}
