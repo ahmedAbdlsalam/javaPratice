9cec25eba7fca50bf47d36d1e3f94a4fcaf3ff7f
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-12 13:20:50 +0300

    Add new problem   Proplem09(CanMakeArithmeticProgressionFromSequence

---
+++ Proplem09(CanMakeArithmeticProgressionFromSequence/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello mode koke");
+    }
+}
