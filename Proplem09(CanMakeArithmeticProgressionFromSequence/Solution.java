dafdeeac0ce10d0ce1a92fa18452aff4de10a3d8
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-12 13:52:47 +0300

    Solution using builtin sort function

--- Proplem09(CanMakeArithmeticProgressionFromSequence/Solution.java
+++ Proplem09(CanMakeArithmeticProgressionFromSequence/Solution.java
@@ -1,10 +1,23 @@
+import java.util.Arrays;
+
 public class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        
+        int[] ardata = arr;
+        Arrays.sort(ardata);
+        int diff = Math.abs(ardata[1]-ardata[0]);
+        for(int i=0; i<ardata.length-1; i++){
+            if(Math.abs(ardata[i]-ardata[i+1]) != diff) return false;
+        }
         return true;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello mode koke");
+        
+
+        int[] arr = {5,1,5,22,-1,4};
+
+        Solution s = new Solution();
+
+        System.out.println(s.canMakeArithmeticProgression(arr));
     }
 }
