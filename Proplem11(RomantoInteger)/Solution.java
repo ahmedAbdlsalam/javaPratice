a494a4e8690f43fc9cc2434f3732836ae9ee4583
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-13 11:13:37 +0300

    Adding Proplem10(Roman to Integer

---
+++ Proplem11(RomantoInteger)/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int romanToInt(String s) {
+     
+        return 1;
+    }    
+
+
+    public static void main(String[] args) {
+        System.out.println("hello moode");
+    }
+}
