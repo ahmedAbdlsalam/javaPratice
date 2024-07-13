fd970524de38c1652868738d04aa9a10440ffd21
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-13 21:16:47 +0300

    Add the Solution of Proplem12

--- Proplem12(LengthofLastWord)/Solution.java
+++ Proplem12(LengthofLastWord)/Solution.java
@@ -1,11 +1,26 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
-        
-        return 1;
+        char [] sArray = s.toCharArray();
+        int lastWordLength =0;
+        boolean catchWord = false;
+
+        for(int i=sArray.length-1; i>=0; i--){
+            if(sArray[i] != ' '){
+                lastWordLength++;
+                catchWord = true;
+            }
+            else if(catchWord){
+                return lastWordLength;
+            }
+        }
+        return lastWordLength;
     }
 
 
     public static void main(String[] args) {
         System.out.println("Hello baby");
+        Solution s1 = new Solution();
+
+        System.out.println(s1.lengthOfLastWord("luffy is still joyboy"));
     }
 }
