0c7ed917a070f651eb4a15f68a2d9e7d591d7b23
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-13 10:33:43 +0300

    Solution with time exceeeded exception!

--- Proplem10(Monotonic Array)/Solution.java
+++ Proplem10(Monotonic Array)/Solution.java
@@ -1,12 +1,36 @@
 public class Solution {
 
     public boolean isMonotonic(int[] nums) {
-        
-        return true;
-    }
+        boolean isMonotonicIncreading = true;
+        boolean isMonotonicDecreasing = true;
 
+        if(nums.length<0 || nums.length > Math.pow(10, 5)) return false;
+        for (int i : nums) {
+            if(i<Math.pow(-10, 5) || i>Math.pow(10, 5)) return false;
+        }
+
+        for(int i=nums.length-1;i>0; i--){
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]<nums[j]) isMonotonicDecreasing = false;
+            }
+        }
+
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]<nums[j])  isMonotonicIncreading = false;
+            }
+        }
+        System.out.println("isMonotonicDecreasing:- "+ isMonotonicDecreasing);
+        System.out.println("isMonotonicIncreading:- "+ isMonotonicIncreading);
+        
+        return (isMonotonicDecreasing || isMonotonicIncreading);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello koke");
+
+        int[] arr = new int[]{1,3,2};
+        Solution s = new Solution();
+        System.out.println(s.isMonotonic(arr));
     }
 }
