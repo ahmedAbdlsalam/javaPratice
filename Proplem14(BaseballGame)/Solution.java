954f619fcd7ca5c0c5f69ef9b9da6b097a078914
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-13 23:04:03 +0300

    Adding new Problem14 Baseball Game

---
+++ Proplem14(BaseballGame)/Solution.java
@@ -0,0 +1,11 @@
+
+public class Solution {
+
+    public int calPoints(String[] operations) {
+     
+        return 1;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello mode");
+    }
+}
