78d80babe1a4e66463b2c6df287045535a76f4de
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-13 20:52:45 +0300

    Adding new Problem11 Length of Last Word

---
+++ Proplem12(LengthofLastWord)/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello baby");
+    }
+}
