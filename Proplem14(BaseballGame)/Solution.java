b7182c0293a0dc39978ffe5806da905732e269ab
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-15 21:03:49 +0300

    Adding new solution using deque interface

--- Proplem14(BaseballGame)/Solution.java
+++ Proplem14(BaseballGame)/Solution.java
@@ -2,33 +2,32 @@ import java.util.*;
 public class Solution {
 
     public int calPoints(String[] operations) {
-    List<Integer> list = new ArrayList<Integer>();
      int result = 0;
-        for(int i=0; i<operations.length; i++){
-            if(operations[i] == "C"){
-                //System.out.println("At clear condition.");
-                list.remove(list.size()-1);
-            }else if(operations[i] == "D"){
-                //System.out.println("At Duplicate condition.");
-                list.add(list.get(list.size()-1)*2);
-            }else if(operations[i] == "+"){
-                //System.out.println("At Addition condition.");
-                list.add( list.get(list.size()-2) + list.get(list.size()-1));
-            }else{
-                //System.out.println("At Else condition.");
-                list.add(Integer.valueOf(operations[i]));
-            }
+     Deque<Integer> stack = new ArrayDeque<>();
+        for (String stringChar : operations) {
+            if(stringChar == "C") stack.removeFirst();
+            else if(stringChar == "D") stack.addFirst(stack.peek()*2);
+            else if(stringChar == "+") {
+                int top = stack.remove(); 
+                int topNew = top + stack.peekFirst(); 
+                stack.addFirst(top);
+                stack.addFirst(topNew);}
 
+            else stack.addFirst(Integer.valueOf(stringChar));
         }
 
-        for (Integer integer : list) {
-            result += integer;
+        Iterator<Integer> i = stack.iterator();
+        while (i.hasNext()) {
+            
+            result += i.next();
+            // System.out.println(result);
+            i.hasNext();
         }
         return result;
     }
     public static void main(String[] args) {
         System.out.println("Hello mode");
-        String[] input = new String[]{"5","2","C","D","+"};
+        String[] input = new String[]{"5","-2","4","C","D","9","+","+"};
         Solution s1 = new Solution();
         System.out.println(s1.calPoints(input));
         
