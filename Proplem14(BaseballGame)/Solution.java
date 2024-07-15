1fd170d949603a31d8c8af647c4dbe1483256434
Ahmed Abdelsalam <ahmedmoabdelsalam98@gmail.com>
2024-07-15 19:51:37 +0300

    Adding a solution that throuing exception one on leetCode!

--- Proplem14(BaseballGame)/Solution.java
+++ Proplem14(BaseballGame)/Solution.java
@@ -1,11 +1,36 @@
-
+import java.util.*; 
 public class Solution {
 
     public int calPoints(String[] operations) {
-     
-        return 1;
+    List<Integer> list = new ArrayList<Integer>();
+     int result = 0;
+        for(int i=0; i<operations.length; i++){
+            if(operations[i] == "C"){
+                //System.out.println("At clear condition.");
+                list.remove(list.size()-1);
+            }else if(operations[i] == "D"){
+                //System.out.println("At Duplicate condition.");
+                list.add(list.get(list.size()-1)*2);
+            }else if(operations[i] == "+"){
+                //System.out.println("At Addition condition.");
+                list.add( list.get(list.size()-2) + list.get(list.size()-1));
+            }else{
+                //System.out.println("At Else condition.");
+                list.add(Integer.valueOf(operations[i]));
+            }
+
+        }
+
+        for (Integer integer : list) {
+            result += integer;
+        }
+        return result;
     }
     public static void main(String[] args) {
         System.out.println("Hello mode");
+        String[] input = new String[]{"5","2","C","D","+"};
+        Solution s1 = new Solution();
+        System.out.println(s1.calPoints(input));
+        
     }
 }
